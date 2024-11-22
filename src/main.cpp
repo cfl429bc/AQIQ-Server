02a6fdf65c314e466da92d0b063b676dda160f88
CFL429BC <londal@bc.edu>
2024-11-22 02:04:38 -0500

    added more

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ int g_PowerLimit = 3000;
 
 // sets variable to switch between Access Point and Sensor board
 // Either "WebServer" or "Mesh"
-String boardType = "WebServer";
+String boardType = "Mesh";
 
 
 // Time taken after visiting /stop
@@ -70,6 +70,18 @@ String keys[6] = {"temperature", "humidity", "pressure", "pm1_0", "pm2_5", "pm10
 int datum[6] = {1, 2, 3, 4, 5, 6};    // pm1.0, pm2.5, pm10.0, temp, hum, psi (placeholder values)
 String suf[6] = {"F", "%", "hPa", "ppm", "ppm", "ppm"};    // Suffixes for readings
 const char* messages[5] = {" ", " ", " ", " ", " "};
+const char * num[100] = {
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
+    "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", 
+    "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", 
+    "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", 
+    "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", 
+    "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", 
+    "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", 
+    "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", 
+    "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", 
+    "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"
+    };
 
 // Sensor data variable 
 // Kinda redundent rn I am just using it for tests
@@ -126,8 +138,7 @@ void serialDelay(int seconds) {
 	Serial.println("");
 	for (int i = 0; i < seconds; i++) {
 		Serial.println(i + 1);
-        updateMessages(num[i]);
-        displayMessages();
+        updateMessages(num[i], true);
 		delay(1000);
 	}
 }
@@ -282,11 +293,11 @@ void receivedCallback(uint32_t from, String &msg) {
 	Serial.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
     
 	for (int i = 0; i < 5; i++) {
-		mapAdd(from, keys[i], doc[keys[i]].as<int>());
+		// mapAdd(from, keys[i], doc[keys[i]].as<int>());
 		// dataMap[from][keys[i]] = doc[keys[i]].as<int>();
 		Serial.print(keys[i]);
         Serial.print(": ");
-        Serial.print(dataMap[from][keys[i]]);
+        // Serial.print(dataMap[from][keys[i]]);
         Serial.print(" ");
         Serial.println(suf[i]);
 	}
